e9e5a954d766233e5ad96644bd97cf1863aaf81f
Anthony Licata <tamamaroro@gmail.com>
2017-07-29 13:23:24 -0400

    Initial commit, going to trash the current player controller

---
+++ Assets/CameraLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLogic : MonoBehaviour {
+
+    public GameObject followObject;
+    public Vector3 followPosition;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updatePosition();
+	}
+
+    void updatePosition()
+    {
+        transform.position = followObject.transform.position + followPosition;
+    }
+}
---
+++ Assets/UIHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour {
+
+    Slider jumpPower;
+
+	// Use this for initialization
+	void Start () {
+        jumpPower = transform.Find("JumpPower").gameObject.GetComponent <Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void updateJumpPower(float value)
+    {
+        jumpPower.value = value;
+    }
+}
